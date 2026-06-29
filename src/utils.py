8d5d2fca429f997e3fef5344304b3bb17dee31eb
Cipher <ajaokudos34@gmail.com>
2026-06-29 13:20:11 +0100

    added a ,

--- src/utils.py
+++ src/utils.py
@@ -12,11 +12,11 @@ scraper_queue = asyncio.Queue()
 async def update_user_info(message: Message, phone_number: str, info: dict):
     timeout = httpx.Timeout(connect=6.0, read=12.0, write=6.0, pool=5.0)
     data = {"phone_number": phone_number, "info": info}
-    params = {'telegram_id':str(message.chat.id)}
+    params = {"telegram_id": str(message.chat.id)}
     async with httpx.AsyncClient(verify=False, timeout=timeout) as client:
         response = await client.post(
             f"{settings.DOMAIN_URL}/update_info",
-            params=params
+            params=params,
             json=data,
         )
         updated = response.json()["Updated"]
